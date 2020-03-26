f49094308db8f5522e4ec9fda8a92081670c55fa
Caroline Carvalho <carvalhocaroline111@gmail.com>
2020-03-26 15:03:59 -0300

    Prepares the project

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex)
+
+new Vuex.Store({
+  state: {
+    products: []
+  },
+  getters: {
+    productsCount() {
+      // ...
+    }
+  },
+  actions: {
+    fetchProducts() {
+      //make the call
+    }
+  },
+  mutations: {
+    setProducts() {
+      //update products
+    }
+  }
+})
