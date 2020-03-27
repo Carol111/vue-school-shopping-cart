c5b281bdd518c329672a95ebfea0cfa1028cbf1c
Caroline Carvalho <carvalhocaroline111@gmail.com>
2020-03-27 08:25:02 -0300

    Vuex getters

--- src/store/index.js
+++ src/store/index.js
@@ -8,8 +8,8 @@ export default new Vuex.Store({
     products: []
   },
   getters: {
-    productsCount() {
-      // ...
+    availableProducts(state, getters) {
+      return state.products.filter(product => product.inventory > 0)
     }
   },
   actions: {
